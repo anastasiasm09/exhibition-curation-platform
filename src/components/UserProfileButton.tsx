c7c172e7da44e79057792b57bec7ad00e7a52773
Anastasiia <anastasiia@MacBook-Pro-13.local>
2025-08-01 17:49:22 +0100

    Added indicator

--- src/components/UserProfileButton.tsx
+++ src/components/UserProfileButton.tsx
@@ -4,7 +4,7 @@ import {
     Portal,
     Dialog,
     Button,
-    CloseButton,
+    CloseButton, HStack, Status,
 } from "@chakra-ui/react"
 import { RiAccountCircleLine } from "react-icons/ri";
 import { useContext, useState } from "react";
@@ -15,6 +15,7 @@ import { AuthContext } from "@/context/AuthContext";
 
 export default function UserProfileButton() {
     const [open, setOpen] = useState<boolean>(false);
+    const [loginStatus, setLoginStatus] = useState<"success" | "error" | null>(null);
     const messageSuccess = "You have successfully logged in.";
     const messageNoCrRes = "No credential received.";
     const messageError = "Login failed";
@@ -26,17 +27,19 @@ export default function UserProfileButton() {
             setGoogleToken(response.credential)
             setOpen(false)
             setIsUserAuthenticated(true)
+            setLoginStatus("success")
             toaster.create({
                 title: messageSuccess,
                 type: "success",
             });
 
         } else if (!response.credential) {
+            setLoginStatus("error")
             toaster.create({
                 title: messageError,
                 type: "error",
             });
-        
+
         } else {
             toaster.create({
                 title: messageNoCrRes,
@@ -45,52 +48,52 @@ export default function UserProfileButton() {
         }
     };
 
-
     const handleError = () => {
-            toaster.create({
-                title: messageError,
-                type: "error",
-            });
-    }; 
+        toaster.create({
+            title: messageError,
+            type: "error",
+        });
+    };
 
 
     return (
         <>
-                <Dialog.Root open={open} onOpenChange={(e) => setOpen(e.open)}>
-                    <Dialog.Trigger asChild>
-                        <IconButton
-                            aria-label="Login"
-                            size="sm"
-                            color="black"
-                            bg="gray.100"
-                        >
-                            <RiAccountCircleLine />
-                        </IconButton>
-                    </Dialog.Trigger>
-                    <Portal>
-                        <Dialog.Backdrop />
-                        <Dialog.Positioner>
-                            <Dialog.Content>
-                                <Dialog.Header>
-                                    <Dialog.Title>Login</Dialog.Title>
-                                </Dialog.Header>
-                                <Dialog.Body>
-                                    <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
-                                </Dialog.Body>
-                                <Dialog.Footer>
-                                    <Dialog.ActionTrigger asChild>
-                                        <Button variant="outline">Cancel</Button>
-                                    </Dialog.ActionTrigger>
-                                </Dialog.Footer>
-                                <Dialog.CloseTrigger asChild>
-                                    <CloseButton size="sm" />
-                                </Dialog.CloseTrigger>
-                            </Dialog.Content>
-                        </Dialog.Positioner>
-                    </Portal>
-                </Dialog.Root>
+            <Dialog.Root open={open} onOpenChange={(e) => setOpen(e.open)}>
+                <Dialog.Trigger asChild>
+                    <IconButton
+                        aria-label="Login"
+                        size="sm"
+                        color={loginStatus === "success" ? "maroon" : "black"}
+                        bg="gray.100"
+                    >
+                        <RiAccountCircleLine />
+                        
+                    </IconButton>
+                </Dialog.Trigger>
+                <Portal>
+                    <Dialog.Backdrop />
+                    <Dialog.Positioner>
+                        <Dialog.Content>
+                            <Dialog.Header>
+                                <Dialog.Title>Login</Dialog.Title>
+                            </Dialog.Header>
+                            <Dialog.Body>
+                                <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+                            </Dialog.Body>
+                            <Dialog.Footer>
+                                <Dialog.ActionTrigger asChild>
+                                    <Button variant="outline">Cancel</Button>
+                                </Dialog.ActionTrigger>
+                            </Dialog.Footer>
+                            <Dialog.CloseTrigger asChild>
+                                <CloseButton size="sm" />
+                            </Dialog.CloseTrigger>
+                        </Dialog.Content>
+                    </Dialog.Positioner>
+                </Portal>
+            </Dialog.Root>
 
-                <Toaster />
+            <Toaster />
         </>
     )
 }
