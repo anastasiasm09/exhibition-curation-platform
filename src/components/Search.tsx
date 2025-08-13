49bd96dcd759b9fce104f009f5f064e72228b415
Anastasiia Samoilova <anastasiasm09@gmail.com>
2025-08-13 22:50:45 +0100

    rewrite Search component in TypeScript

---
+++ src/components/Search.tsx
@@ -0,0 +1,69 @@
+import {
+    Input,
+    InputGroup,
+    CloseButton,
+} from '@chakra-ui/react';
+import { LuSearch } from "react-icons/lu";
+import { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type SearchProps = {
+    onSearch: (query: string) => void;
+    onFinish?: () => void;
+}
+
+export default function Search({ onSearch, onFinish }: SearchProps) {
+    const [search, setSearch] = useState<string>("");
+    const navigate = useNavigate();
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleSearch = () => {
+        onSearch(search.trim());
+
+        if (onFinish) {
+            onFinish();
+        }
+    };
+
+    function handleKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
+        if (e.key === 'Enter') {
+            if (window.location.pathname !== "/") {
+                navigate("/")
+            }
+            handleSearch();
+        }
+    };
+
+    const endElement = search ? (
+        <CloseButton
+            size="xs"
+            onClick={() => {
+                setSearch("");
+                onSearch("");
+                inputRef.current?.focus()
+            }}
+            me="-2"
+        />
+    ) : undefined
+
+    return (
+        <>
+            <InputGroup
+                startElement={<LuSearch />}
+                endElement={endElement}
+            >
+                <Input
+                    ref={inputRef}
+                    flex="1"
+                    w="400px"
+                    type="text"
+                    placeholder='Search by artwork, artist or keywords'
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    bg="gray.100"
+                />
+            </InputGroup>
+        </>
+    )
+}
