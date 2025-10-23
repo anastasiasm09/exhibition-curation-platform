e2cf1c8a48add6406d73ad0f59f5962b9634ed7e
Anastasiia Samoilova <anastasiasm09@gmail.com>
2025-10-23 21:52:22 +0100

    add button to delete artwork from exhibition

--- src/components/ExhibitionDetails.tsx
+++ src/components/ExhibitionDetails.tsx
@@ -1,25 +1,29 @@
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
-import { Box, Text, Image, SimpleGrid, Card, CardBody, CardTitle, Flex, Heading } from '@chakra-ui/react';
+import { Box, Text, Image, SimpleGrid, Card, CardBody, CardTitle, Flex, Heading, IconButton, Dialog, Portal, Button, CloseButton, Grid, CardFooter } from '@chakra-ui/react';
 import ArtworkDialog from "./ArtworkDialog";
-import { getEhibitionDetails, markAnExhibitionAsShared, stopSharingAnExhibition } from "@/utils/Exhibitions";
+import { deleteArtworkFromExhibition, getEhibitionDetails, markAnExhibitionAsShared, stopSharingAnExhibition } from "@/utils/Exhibitions";
 import { Icon, Switch } from "@chakra-ui/react"
 import { IoMdLock, IoMdUnlock } from "react-icons/io";
 import { useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
 import { Artwork } from "@/models/Artwork";
 import type { ExhibitionDetails } from "@/models/Exhibition.ts";
+import { MdDeleteOutline } from "react-icons/md";
+import { toaster } from "@/components/ui/toaster"
 
 
 export default function ExhibitionDetails() {
     const { id } = useParams() as { id: string };
-   
+
     const [openDialog, setOpenDialog] = useState<boolean>(false);
     const [selectedArtwork, setSelectedArtwork] = useState<Artwork | null>(null);
     const [exhibitionDetails, setExhibitionDetails] = useState<ExhibitionDetails>();
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [isNotFound, setIsNotFound] = useState<boolean>(false);
     const [isShared, setIsShared] = useState<boolean>(false);
+    const [artworkToDelete, setArtworkToDelete] = useState<string>();
+    const [deleteOpenDialog, setDeleteOpenDialog] = useState<boolean>(false);
 
     const { isUserAuthenticated } = useContext(AuthContext);
 
@@ -62,26 +66,26 @@ export default function ExhibitionDetails() {
         return errorMessage("Exhibition not found");
     }
 
-    if(!exhibitionDetails) return;
+    if (!exhibitionDetails) return;
 
     function handleSwitch() {
         if (!isUserAuthenticated) return null;
-      
+
         return (
-          <Switch.Root colorPalette="black" size="lg" checked={isShared} onCheckedChange={handleShared}>
-            <Switch.HiddenInput />
-            <Switch.Control>
-              <Switch.Thumb />
-              <Switch.Indicator fallback={<Icon as={IoMdLock} color="gray.400" />}>
-                <Icon as={IoMdUnlock} color="white" />
-              </Switch.Indicator>
-            </Switch.Control>
-            <Switch.Label>
-              {isShared ? "Public" : "Private"}
-            </Switch.Label>
-          </Switch.Root>
+            <Switch.Root colorPalette="black" size="lg" checked={isShared} onCheckedChange={handleShared}>
+                <Switch.HiddenInput />
+                <Switch.Control>
+                    <Switch.Thumb />
+                    <Switch.Indicator fallback={<Icon as={IoMdLock} />}>
+                        <Icon as={IoMdUnlock} color="white" />
+                    </Switch.Indicator>
+                </Switch.Control>
+                <Switch.Label>
+                    {isShared ? "Public" : "Private"}
+                </Switch.Label>
+            </Switch.Root>
         );
-      }
+    }
 
     function handleShared() {
         if (isShared) {
@@ -93,6 +97,30 @@ export default function ExhibitionDetails() {
         }
     }
 
+    function handleArtworkDelete() {
+        if (!artworkToDelete?.trim()) return;
+
+        deleteArtworkFromExhibition(id, artworkToDelete)
+            .then(() => {
+                setExhibitionDetails(prev => {
+                    if (!prev) return prev;
+
+                    return {
+                        ...prev,
+                        artworks: prev.artworks.filter(a => a.id !== artworkToDelete)
+                    };
+                });
+
+                setDeleteOpenDialog(false);
+                setArtworkToDelete('');
+            })
+            .catch(err => console.error(err));
+
+        toaster.create({
+            description: "Artwork deleted successfully",
+            type: "info",
+        })
+    };
 
     return (
         <>
@@ -129,16 +157,67 @@ export default function ExhibitionDetails() {
                                 }}
                             />
                             <CardBody>
-                                <CardTitle>{artwork.title}</CardTitle>
+                                <Grid templateColumns="1fr auto" alignItems="start">
+
+                                    <CardTitle>{artwork.title}</CardTitle>
+                                   
+                                </Grid>
                                 <Card.Description>{artwork.artist}</Card.Description>
                                 <Text textStyle="sm" fontWeight="normal" letterSpacing="tight" mt="2">
                                     {artwork.classification}
                                 </Text>
                             </CardBody>
+                            <CardFooter>
+                                     <Box>
+                                        <IconButton
+                                            aria-label="Delete artwork from an exhibition"
+                                            size="md"
+                                            display={{ base: 'none', md: 'flex' }}
+                                            color="maroon"
+                                            variant="plain"
+                                            colorScheme="none"
+                                            onClick={() => {
+                                                setDeleteOpenDialog(true)
+                                                setArtworkToDelete(artwork.id)
+                                            }}
+                                        >
+                                            <MdDeleteOutline />
+                                            <Text>Delete artwork</Text>
+                                        </IconButton>
+                                    </Box>
+                                </CardFooter>
                         </Card.Root>
                     ))}
                 </SimpleGrid>
             )}
+
+            {/* Dialog for delete artwork */}
+            <Dialog.Root open={deleteOpenDialog} onOpenChange={(e) => setDeleteOpenDialog(e.open)}>
+                <Portal>
+                    <Dialog.Backdrop />
+                    <Dialog.Positioner>
+                        <Dialog.Content>
+                            <Dialog.Header>
+                                <Dialog.Title color="maroon">Are you sure you want to delete this artwork from the exhibition?</Dialog.Title>
+                            </Dialog.Header>
+                            <Dialog.Footer>
+                                <Dialog.ActionTrigger asChild>
+                                    <Button variant="outline">Cancel</Button>
+                                </Dialog.ActionTrigger>
+                                <Button
+                                    bg="maroon"
+                                    onClick={handleArtworkDelete}
+                                >
+                                    Yes
+                                </Button>
+                            </Dialog.Footer>
+                            <Dialog.CloseTrigger asChild>
+                                <CloseButton variant="plain" size="sm" />
+                            </Dialog.CloseTrigger>
+                        </Dialog.Content>
+                    </Dialog.Positioner>
+                </Portal>
+            </Dialog.Root>
 
             <ArtworkDialog
                 artwork={selectedArtwork}
